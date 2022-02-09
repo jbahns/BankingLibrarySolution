71ab386f7fdb4fb742f2da3dd80f377de1337010
jbahns <javibahns@gmail.com>
2022-02-09 11:34:18 -0500

    Add project files.

---
+++ TestBanking/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BankingLibrary;
+
+namespace TestBankingLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account();
+            Console.WriteLine(account.Balance);
+            account.CalculateInterest(12);
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
